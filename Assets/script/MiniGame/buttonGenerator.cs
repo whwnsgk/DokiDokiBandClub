6f5ec9ed901962d92fc1184f57e71402cd1a55b5
DESKTOP-MLQVVMG\good <whwnsgk94@naver.com>
2022-02-09 00:06:08 +0900

    리듬게임 만들기 위한 요소
    - 아직 많이 해야함

---
+++ Assets/script/MiniGame/buttonGenerator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonGenerator : MonoBehaviour
+{
+    public GameObject buttonPrefab;
+    float span = 1.0f ;
+    float delta = 0;
+    
+
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if(this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject go = Instantiate(buttonPrefab) as GameObject;
+            int px = Random.Range(-6, 7);
+            go.transform.position = new Vector3(px, 7, 0);
+        }
+    }
+}
